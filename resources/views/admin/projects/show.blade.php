05a5b9b220dd57074201ad714f4ee14924d7d869
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-09-01 17:24:49 +0200

    change details on show page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center">{{ $project->title }}</h1>
-
-    <div>
-        <p>{{ $project->content }}</p>
-        <img src="{{ $project->image }}" alt=" {{ $project->title }} ">
+    <div class="row mt-3">
+        <div class="col-3">
+            <img src="{{ $project->image }}" alt=" {{ $project->title }} " class="img-fluid">
+        </div>
+        <div class="col">
+            <h1 class="text-center">{{ $project->title }}</h1>
+            <p>{{ $project->content }}</p>
+        </div>
     </div>
 
-    <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary">Back</a>
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-secondary mt-3">Back</a>
 @endsection
